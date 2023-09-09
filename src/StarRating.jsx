eadf610105fd2a22ecdd49982b032e267ca96a27
OlusegunAdewale <adewalesegun76@gmail.com>
2023-09-09 02:15:16 +0100

    Initial commit

---
+++ src/StarRating.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+function StarRating({ rating }) {
+   // Here's the markup for a single star://
+   const stars = [];
+   
+   for(let i = 0; i < rating; i++) {
+     stars.push(
+
+    <img
+      alt=""
+      className="gold-star"
+      src="https://sandpack-bundler.vercel.app/img/gold-star.svg"
+    />
+    );
+  }
+    
+    /*Your job is to repeat this element
+    based on the `rating` prop.
+    If the rating is 4, we need 4 copies.*/
+
+  return <div className="star-wrapper">{stars}</div>;
+}
+
+export default StarRating;
